16cd6c2a6e329f14ef21675a9b8642d7ac223d26
Thais Poentes <thais_poentes@hotmail.com>
2020-09-04 03:20:09 -0300

    criando variaveis

--- src/com/bytebank/main.kt
+++ src/com/bytebank/main.kt
@@ -1,3 +1,11 @@
 fun main() {
-	println("Oie")
+	
+	//variavel que o valor pode ser modificado
+	var oi = 'O'
+	
+	//variavel que NAO pode ser modificada
+	val eai = "Oieeee"
+	
+	println("Teste de variavel: $eai $oi")
+
 }
